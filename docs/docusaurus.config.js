1e574d06f8efe430b8dd43b53be7441cbb4255e8
נυαη נυαηѕση <john@dewey.ws>
2023-12-30 11:21:09 -0800

    chore: prepare for v2 release

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -115,7 +115,7 @@ const config = {
       },
       announcementBar: {
         id: 'announcementBar-3', // Increment on change
-        content: `🎉️ Gilt is 💯 rearchitected. <b><a target="_blank" href="https://github.com/retr0h/go-gilt/releases/tag/v1.0.2">Gilt v1.0.2</a> is now out!</b> 🥳️`,
+        content: `🎉️ Gilt has been 💯 rewritten in Go, <b><a target="_blank" href="https://github.com/retr0h/go-gilt/releases/tag/v2.0">v2.0</a> is now available!</b> 🥳️`,
       },
     }),
 };
