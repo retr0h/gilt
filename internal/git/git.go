b973dd8ffc5ad9b8b41e949a1ffc5a3c2db4e187
נυαη נυαηѕση <john@dewey.ws>
2024-03-07 21:17:50 -0800

    chore: coverage++
    Increased coverage on git worktree.

--- internal/git/git.go
+++ internal/git/git.go
@@ -31,6 +31,13 @@ import (
 	"github.com/retr0h/gilt/v2/internal"
 )
 
+// AbsFn function to switch when testing.
+// The only time `Abs` will return an error is when `os.Getwd()` returns an
+// error.  Given this situation is rare, but "it's a weird, wild world out there"
+// opted to retain he error handling, but clumsily swap the test function for
+// coverage++.
+var AbsFn = (*Git).abs
+
 // New factory to create a new Git instance.
 func New(
 	appFs avfs.VFS,
@@ -69,7 +76,7 @@ func (g *Git) Worktree(
 	version string,
 	dstDir string,
 ) error {
-	dst, err := g.appFs.Abs(dstDir)
+	dst, err := AbsFn(g, dstDir)
 	if err != nil {
 		return err
 	}
@@ -93,4 +100,8 @@ func (g *Git) Worktree(
 		_ = g.execManager.RunCmdInDir("git", []string{"worktree", "prune", "--verbose"}, cloneDir)
 	}
 	return err
+}
+
+func (g *Git) abs(path string) (string, error) {
+	return g.appFs.Abs(path)
 }
